c4405a7ec58a516cb08bb977319b6b670f2f1001
Gonzalo Rancor <rancorgonzalo@gmail.com>
2023-02-21 21:02:16 -0300

    primer-commit

---
+++ js/js.js
@@ -0,0 +1,85 @@
+let contraseña = "matecito";
+let usuario = "Gon";
+
+function login() {
+  let ingresar = false;
+
+  for (let i = 2; i >= 0; i--) {
+    let userPass = prompt("Ingresá tu contraseña.");
+    if (userPass === contraseña) {
+      ingresar = true;
+      break;
+    } else {
+      alert("Contraseña invalida.");
+    }
+  }
+  return ingresar;
+}
+if (login()) {
+  alert("Ingreso exitoso. Bienvenido " + usuario);
+  let onixNegro = document.getElementById("btnOnix");
+  btnOnix.addEventListener("click", function cantidad() {
+    var respuesta = parseInt(
+      prompt("Ingrese la cantidad de unidades que desea comprar")
+    );
+    alert(
+      respuesta +
+        " Japa Mala de Onix negro se ha agregado al carrito de compras"
+    );
+  });
+
+  let btnPulcera = document.getElementById("btnPulcera");
+  btnPulcera.addEventListener("click", function cantidad() {
+    var respuesta = parseInt(prompt("Ingrese cuantas pulceras quiere"));
+    alert(respuesta + " pulcera se ha agregado al carrito de compras");
+  });
+
+  let btnMadera = document.getElementById("btnMadera");
+  btnMadera.addEventListener("click", function cantidad() {
+    var respuesta = parseInt(prompt("Ingrese cuantas pulceras quiere"));
+    alert(
+      respuesta + " Japa Mala de madera se ha agregado al carrito de compras"
+    );
+  });
+
+  let btnCorazon = document.getElementById("btnCorazon");
+  btnCorazon.addEventListener("click", function cantidad() {
+    var respuesta = parseInt(prompt("Ingrese cuantas pulceras quiere"));
+    alert(
+      respuesta + " Japa Mala corazon se ha agregado al carrito de compras"
+    );
+  });
+} else {
+  alert("Supero el numero de intentos permitidos. Intente en un rato.");
+}
+/*
+let onixNegro = document.getElementById("btnOnix");
+btnOnix.addEventListener("click", function cantidad() {
+  var respuesta = parseInt(
+    prompt("Ingrese la cantidad de unidades que desea comprar")
+  );
+  alert(
+    respuesta + " Japa Mala de Onix negro se ha agregado al carrito de compras"
+  );
+});
+
+let btnPulcera = document.getElementById("btnPulcera");
+btnPulcera.addEventListener("click", function cantidad() {
+  var respuesta = parseInt(prompt("Ingrese cuantas pulceras quiere"));
+  alert(respuesta + " pulcera se ha agregado al carrito de compras");
+});
+
+let btnMadera = document.getElementById("btnMadera");
+btnMadera.addEventListener("click", function cantidad() {
+  var respuesta = parseInt(prompt("Ingrese cuantas pulceras quiere"));
+  alert(
+    respuesta + " Japa Mala de madera se ha agregado al carrito de compras"
+  );
+});
+
+let btnCorazon = document.getElementById("btnCorazon");
+btnCorazon.addEventListener("click", function cantidad() {
+  var respuesta = parseInt(prompt("Ingrese cuantas pulceras quiere"));
+  alert(respuesta + " Japa Mala corazon se ha agregado al carrito de compras");
+});
+*/
